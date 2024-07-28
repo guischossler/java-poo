2e59ffab27047faf263105116697d22eb6a9e287
guischossler <g521g99@gmail.com>
2024-07-28 17:55:22 -0300

    aprendendo interfaces

---
+++ EstudoInterfaces/src/estudointerfaces/ControleRemoto.java
@@ -0,0 +1,124 @@
+package estudointerfaces;
+
+public class ControleRemoto implements Controlador {
+
+    // Atributos
+    private int volume;
+    private boolean ligado;
+    private boolean tocando;
+
+    // Metodos especiais
+    public ControleRemoto() { // construtor
+        this.setVolume(50);
+        this.setLigado(true);
+        this.setTocando(false);
+
+    }
+
+    // Getters e Setters
+    private int getVolume() {
+        return volume;
+    }
+
+    private void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    private boolean isLigado() {
+        return ligado;
+    }
+
+    private void setLigado(boolean ligado) {
+        this.ligado = ligado;
+    }
+
+    private boolean isTocando() {
+        return tocando;
+    }
+
+    private void setTocando(boolean tocando) {
+        this.tocando = tocando;
+    }
+
+    @Override
+    public void ligar() {
+        this.setLigado(true);
+    }
+
+    @Override
+    public void desligar() {
+        this.setLigado(false);
+    }
+
+    @Override
+    public void abrirMenu() {
+        if (isLigado()) {
+            System.out.println("-----Menu-----");
+            System.out.println("Está ligado? " + this.isLigado());
+            System.out.println("Está tocando? " + this.isTocando());
+            System.out.print("Volume: " + this.getVolume());
+            for (int i = 0; i <= this.getVolume(); i += 10) {
+                System.out.print("|");
+            }
+            System.out.println("");
+        } else {
+            System.out.println("Impossivel abrir menu");
+        }
+    }
+
+    @Override
+    public void fecharMenu() {
+        System.out.println("Fechando menu");
+    }
+
+    @Override
+    public void maisVolume() {
+        if (this.isLigado()) {
+            this.setVolume(this.getVolume() + 5);
+        } else {
+            System.out.println("Impossível aumentar volume");
+        }
+    }
+
+    @Override
+    public void menosVolume() {
+        if (this.isLigado()) {
+            this.setVolume(this.getVolume() - 5);
+        } else {
+            System.out.println("Impossível diminuir volume");
+        }
+    }
+
+    @Override
+    public void ligarMudo() {
+        if (this.isLigado() && getVolume() > 0) {
+            this.setVolume(0);
+        }
+    }
+
+    @Override
+    public void desligarMudo() {
+        if (this.isLigado() && getVolume() == 0) {
+            this.setVolume(50);
+        }
+    }
+
+    @Override
+    public void play() {
+        if (this.isLigado() && !(this.isTocando())) {
+            // se está ligado e não está tocando
+            this.setTocando(true);
+        } else {
+            System.out.println("Não consegui reproduzir");
+        }
+    }
+
+    @Override
+    public void pause() {
+        if (this.isLigado() && this.isTocando()) {
+            this.setTocando(false);
+        } else {
+            System.out.println("Não consegui pausar");
+        }
+    }
+}
---
+++ EstudoInterfaces/src/estudointerfaces/Main.java
@@ -0,0 +1,14 @@
+package estudointerfaces;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        ControleRemoto c = new ControleRemoto();
+        
+        c.play();
+        c.abrirMenu();
+        c.fecharMenu();
+    }
+
+}
